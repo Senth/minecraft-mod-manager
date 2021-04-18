57e569bae1ced103f7b4dcfebce5630b8c66f411
Matteus Magnusson <senth.wallace@gmail.com>
2021-04-18 09:28:18 +0200

    ✅ Add test for get_mods()

--- minecraft_mod_manager/gateways/dir_parser_test.py
+++ minecraft_mod_manager/gateways/dir_parser_test.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 
-import pytest
-from mockito import unstub, verifyStubbedInvocationsAreUsed, when
-from mockito.mockito import expect
-
 from ..core.entities.mod import Mod
 from ..core.entities.mod_loaders import ModLoaders
 from .dir_parser import DirParser
 
+carpet_filename = "fabric-carpet-1.16.4-1.4.16+v201105.jar"
+jei_filename = "jei-1.16.5-7.6.4.86.jar"
+mod_menu_filename = "modmenu-1.14.13+build.19.jar"
+
 
 def test_get_mod_info_when_mod_is_fabric():
-    filename = "fabric-carpet-1.16.4-1.4.16+v201105.jar"
+    filename = carpet_filename
     input = Path(f"fixtures/{filename}")
     expected = Mod("carpet", "Carpet Mod in Fabric", version="1.4.16", mod_loader=ModLoaders.fabric, file=filename)
 
@@ -20,7 +20,7 @@ def test_get_mod_info_when_mod_is_fabric():
 
 
 def test_get_mod_info_when_mod_is_forge():
-    filename = "jei-1.16.5-7.6.4.86.jar"
+    filename = jei_filename
     input = Path(f"fixtures/{filename}")
     expected = Mod("jei", "Just Enough Items", version="7.6.4.86", mod_loader=ModLoaders.forge, file=filename)
 
@@ -35,5 +35,18 @@ def test_no_mod_info_from_invalid_mod():
     expected = None
 
     result = DirParser.get_mod_info(input)
+
+    assert expected == result
+
+
+def test_get_mods():
+    input = Path("fixtures")
+    expected = [
+        Mod("carpet", "Carpet Mod in Fabric", version="1.4.16", mod_loader=ModLoaders.fabric, file=carpet_filename),
+        Mod("jei", "Just Enough Items", version="7.6.4.86", mod_loader=ModLoaders.forge, file=jei_filename),
+        Mod("modmenu", "Mod Menu", version="1.14.13+build.19", mod_loader=ModLoaders.fabric, file=mod_menu_filename),
+    ]
+
+    result = DirParser.get_mods(input)
 
     assert expected == result
