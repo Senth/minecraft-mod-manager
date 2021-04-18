d16e633c17e86afdd14adf9d470f5e706840bbc0
Matteus Magnusson <senth.wallace@gmail.com>
2021-04-18 10:33:44 +0200

    ♻ ✅ 🚚 Move out arg parser from config and add tests

--- minecraft_mod_manager/config.py
+++ minecraft_mod_manager/config.py
@@ -1,21 +1,8 @@
-import argparse
-import re
-from os import path
 from typing import Any, List, Literal
 
-from .core.entities.mod import ModArg, RepoTypes
+from .core.entities.mod import ModArg
 
 _app_name = __package__.replace("_", "-")
-
-_red_color = "\033[91m"
-_no_color = "\033[0m"
-
-
-def _is_dir(dir: str) -> str:
-    if path.isdir(dir):
-        return dir
-    else:
-        raise NotADirectoryError(dir)
 
 
 class Config:
@@ -28,71 +15,13 @@ class Config:
         self.minecraft_version: str or None
         self.beta: bool = False
         self.alpha: bool = False
-        self.action: Literal["install", "update"]
-        self.mods: List[ModArg] = []
+        self.action: Literal["install", "update", "configure", "list"]
+        self.arg_mods: List[ModArg] = []
 
-    def _parse_args(self):
-        # Get arguments first to get verbosity before we get everything else
-        parser = argparse.ArgumentParser(description="Install or update Minecraft mods from Curseforge")
-
-        parser.add_argument(
-            "action",
-            choices=["install", "update", "configure", "list"],
-            help="Install, update, configure, or list mods",
-        )
-        parser.add_argument(
-            "mods",
-            nargs="*",
-            help="The mods to install, update, or configure. "
-            + "If no mods are specified during an update, all mods will be updated. "
-            + "\nTo specify the download site for the mod you can put 'site:' before the mod. "
-            + "E.g. 'curse:litematica' By default it searches all sites for the mod.\n"
-            + "To configure an alias for the mod, use 'mod_name=ALIAS_NAME'. E.g. 'dynmap=dynmapforge'",
-        )
-        parser.add_argument(
-            "-d",
-            "--dir",
-            type=_is_dir,
-            help="Location of the mods folder. By default it's the current directory",
-        )
-        parser.add_argument(
-            "-v",
-            "--minecraft-version",
-            help="Only update mods to this Minecraft version",
-        )
-        parser.add_argument(
-            "--allow-beta",
-            action="store_true",
-            help="Allow beta releases",
-        )
-        parser.add_argument(
-            "--allow-alpha",
-            action="store_true",
-            help="Allow alpha and beta releases",
-        )
-        parser.add_argument(
-            "--verbose",
-            action="store_true",
-            help="Print more messages",
-        )
-        parser.add_argument(
-            "--debug",
-            action="store_true",
-            help="Turn on debug messages. This automatically turns on --verbose as well",
-        )
-        parser.add_argument(
-            "--pretend",
-            action="store_true",
-            help="Only pretend to update/configure. Does not change anything",
-        )
-
-        _args = parser.parse_args()
-        self._add_args_settings(_args)
-
-    def _add_args_settings(self, args: Any):
+    def add_arg_settings(self, args: Any):
         """Set additional configuration from script arguments
 
-        Args:
+        args:
             args (list): All the parsed arguments
         """
         self.action = args.action
@@ -112,34 +41,7 @@ class Config:
         if args.allow_alpha:
             self.beta = True
 
-        # Process mods
-        self.mods = []
-
-        for mod_arg in args.mods:
-            match = re.match(r"(?:(.+):)?([\w-]+)(?:=(.+))?", mod_arg)
-
-            if not match:
-                print(f"{_red_color}Invalid mod syntax: {mod_arg}{_no_color}")
-                exit(1)
-
-            repo_type_name, mod_id, repo_alias = match.groups()
-            if repo_type_name and len(repo_type_name) > 0:
-                try:
-                    repo_type = RepoTypes[repo_type_name.lower()]
-                except KeyError:
-                    print(f"{_red_color}No site named {repo_type_name}{_no_color}")
-                    print("Valid names are:")
-                    for enum in RepoTypes:
-                        print(f"{enum.value}")
-
-                    exit(1)
-            else:
-                repo_type = RepoTypes.unknown
-
-            if not repo_alias:
-                repo_alias = mod_id
-
-            self.mods.append(ModArg(repo_type, mod_id, repo_alias))
+        self.arg_mods = args.mods
 
 
 global config
---
+++ minecraft_mod_manager/core/entities/actions.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Union
+
+
+class Actions(Enum):
+    install = "install"
+    update = "update"
+    configure = "configure"
+    list = "list"
+
+    @staticmethod
+    def from_name(name: str) -> Union[Actions, None]:
+        for action in Actions:
+            if action.value == name:
+                return action
+
+    @staticmethod
+    def get_all_names_as_list() -> List[str]:
+        names: List[str] = []
+        for action in Actions:
+            names.append(action.value)
+        return names
