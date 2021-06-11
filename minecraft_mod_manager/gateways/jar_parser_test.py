08c78b9c470969cc3d69b4d94fc93e7772f3a94c
Matteus Magnusson <senth.wallace@gmail.com>
2021-06-11 23:57:58 +0200

    🐛 Can load mod-info when json is malformatted
    Fix #53

--- minecraft_mod_manager/gateways/jar_parser_test.py
+++ minecraft_mod_manager/gateways/jar_parser_test.py
@@ -6,10 +6,41 @@ from ..core.entities.mod import Mod
 from ..core.entities.mod_loaders import ModLoaders
 from .jar_parser import JarParser
 
-carpet_filename = "fabric-carpet-1.16.4-1.4.16+v201105.jar"
-jei_filename = "jei-1.16.5-7.6.4.86.jar"
-mod_menu_filename = "modmenu-1.14.13+build.19.jar"
-capes_filename = "fabric-invalid-character.jar"
+jei = Mod(
+    "jei",
+    "Just Enough Items",
+    version="7.6.4.86",
+    mod_loader=ModLoaders.forge,
+    file="jei-1.16.5-7.6.4.86.jar",
+)
+carpet = Mod(
+    "carpet",
+    "Carpet Mod in Fabric",
+    version="1.4.16",
+    mod_loader=ModLoaders.fabric,
+    file="fabric-carpet-1.16.4-1.4.16+v201105.jar",
+)
+mod_menu = Mod(
+    "modmenu",
+    "Mod Menu",
+    version="1.14.13+build.19",
+    mod_loader=ModLoaders.fabric,
+    file="modmenu-1.14.13+build.19.jar",
+)
+capes = Mod(
+    "capes",
+    "Capes",
+    version="1.1.2",
+    mod_loader=ModLoaders.fabric,
+    file="fabric-invalid-character.jar",
+)
+item_model_fix = Mod(
+    "itemmodelfix",
+    "Item Model Fix",
+    version="1.0.2+1.17",
+    mod_loader=ModLoaders.fabric,
+    file="fabric-invalid-control-character.jar",
+)
 
 
 def path(filename: str) -> Path:
@@ -21,25 +52,13 @@ def path(filename: str) -> Path:
     [
         (
             "Returns mod info when fabric",
-            carpet_filename,
-            Mod(
-                "carpet",
-                "Carpet Mod in Fabric",
-                version="1.4.16",
-                mod_loader=ModLoaders.fabric,
-                file=carpet_filename,
-            ),
+            carpet.file,
+            carpet,
         ),
         (
             "Returns mod info when forge",
-            jei_filename,
-            Mod(
-                "jei",
-                "Just Enough Items",
-                version="7.6.4.86",
-                mod_loader=ModLoaders.forge,
-                file=jei_filename,
-            ),
+            jei.file,
+            jei,
         ),
         (
             "Returns no mod info when invalid mod",
@@ -48,14 +67,13 @@ def path(filename: str) -> Path:
         ),
         (
             "Returns valid mod info when contains invalid characters",
-            capes_filename,
-            Mod(
-                "capes",
-                "Capes",
-                version="1.1.2",
-                mod_loader=ModLoaders.fabric,
-                file=capes_filename,
-            ),
+            capes.file,
+            capes,
+        ),
+        (
+            "Returns valid mod info when it has an invalid control character",
+            item_model_fix.file,
+            item_model_fix,
         ),
     ],
 )
@@ -69,12 +87,7 @@ def test_get_mod_info(name, file, expected):
 
 def test_get_mods():
     input = Path("fixtures").joinpath("mods")
-    expected = [
-        Mod("jei", "Just Enough Items", version="7.6.4.86", mod_loader=ModLoaders.forge, file=jei_filename),
-        Mod("carpet", "Carpet Mod in Fabric", version="1.4.16", mod_loader=ModLoaders.fabric, file=carpet_filename),
-        Mod("modmenu", "Mod Menu", version="1.14.13+build.19", mod_loader=ModLoaders.fabric, file=mod_menu_filename),
-        Mod("capes", "Capes", version="1.1.2", mod_loader=ModLoaders.fabric, file=capes_filename),
-    ]
+    expected = [jei, carpet, mod_menu, capes, item_model_fix]
     jar_parser = JarParser(input)
     result = jar_parser.mods
 
