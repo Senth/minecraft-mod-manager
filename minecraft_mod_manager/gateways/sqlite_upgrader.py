a6c75206f3e142f8caef42bf49aaf14e22971cc6
Senth <senth.wallace@gmail.com>
2021-06-14 00:21:54 +0200

    🚧 Now upgrades to db.v2 with multiple sites

--- minecraft_mod_manager/gateways/sqlite_upgrader.py
+++ minecraft_mod_manager/gateways/sqlite_upgrader.py
@@ -27,7 +27,7 @@ class _Column:
 
 
 class SqliteUpgrader:
-    _version = 1
+    _version = 2
 
     def __init__(self, connection: sqlite3.Connection, cursor: sqlite3.Cursor) -> None:
         self._connection = connection
@@ -46,6 +46,12 @@ class SqliteUpgrader:
         # Upgrade tables one version at a time
         if version <= 0:
             self._v0_to_v1()
+        if version <= 1:
+            self._v1_to_v2()
+
+        # Update version
+        self._cursor.execute("UPDATE version SET version=?", [SqliteUpgrader._version])
+        self._connection.commit()
 
     def _get_version(self) -> int:
         if self._version_table_exists():
@@ -62,23 +68,53 @@ class SqliteUpgrader:
         return self._cursor.fetchone()[0] == 1
 
     def _create_mod_table(self):
+        self._create_mod_table_v2()
+
+    def _create_mod_table_v1(self):
         self._connection.execute(
             "CREATE TABLE IF NOT EXISTS mod ("
-            + f"{_Column.c_id} TEXT UNIQUE, "
-            + f"{_Column.c_site} TEXT, "
-            + f"{_Column.c_site_id} TEXT, "
-            + f"{_Column.c_site_slug} TEXT, "
-            + f"{_Column.c_upload_time} INTEGER, "
-            + f"{_Column.c_active} INTEGER)"
+            + "id TEXT UNIQUE, "
+            + "site TEXT, "
+            + "site_id TEXT, "
+            + "site_slug TEXT, "
+            + "upload_time INTEGER, "
+            + "active INTEGER)"
+        )
+
+    def _create_mod_table_v2(self):
+        self._connection.execute(
+            "CREATE TABLE IF NOT EXISTS mod ("
+            + "id TEXT UNIQUE, "
+            + "sites TEXT, "
+            + "upload_time INTEGER, "
+            + "active INTEGER)"
         )
 
     def _create_version_table(self):
-        self._connection.execute("CREATE TABLE IF NOT EXISTS version (version INTEGER)")
-        self._connection.execute("INSERT INTO version (version) VALUES (?)", [SqliteUpgrader._version])
+        self._cursor.execute("CREATE TABLE IF NOT EXISTS version (version INTEGER)")
+        self._cursor.execute("INSERT INTO version (version) VALUES (?)", [SqliteUpgrader._version])
 
     def _v0_to_v1(self):
         # Just remove mod table and create it again...
-        self._connection.execute("DROP TABLE mod")
-        self._create_mod_table()
+        self._cursor.execute("DROP TABLE mod")
+        self._create_mod_table_v1()
         self._create_version_table()
+        self._connection.commit()
+
+    def _v1_to_v2(self):
+        self._cursor.execute("SELECT * FROM mod")
+        rows = self._cursor.fetchall()
+        self._cursor.execute("DROP TABLE mod")
+        self._create_mod_table_v2()
+
+        for row in rows:
+            id, site, site_id, site_slug, upload_time, active = row
+
+            combined_site = ""
+            if site:
+                combined_site = f"{site}:{site_id}:{site_slug}"
+
+            new = (id, combined_site, upload_time, active)
+            self._cursor.execute("INSERT INTO mod (id, sites, upload_time, active) VALUES (?, ?, ?, ?)", new)
+
         self._connection.commit()
