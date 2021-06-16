1b5e42ea721fed7205209bbf5ea92c23f485df8a
Senth <senth.wallace@gmail.com>
2021-06-16 10:05:19 +0200

    🐛 🔈 Don't remove mods that are up-to-date

--- minecraft_mod_manager/app/update/update_test.py
+++ minecraft_mod_manager/app/update/update_test.py
@@ -1,9 +1,13 @@
-from typing import List
+from typing import List, Union
 
 import pytest
+from minecraft_mod_manager.app.download.download import DownloadInfo
 from mockito import mock, unstub, verifyStubbedInvocationsAreUsed, when
 
+from ...config import config
 from ...core.entities.mod import Mod
+from ...core.entities.sites import Sites
+from ...core.entities.version_info import Stabilities, VersionInfo
 from .update import Update
 from .update_repo import UpdateRepo
 
@@ -32,5 +36,77 @@ def test_call_find_download_and_install(mock_repo):
 
     update.execute([])
 
+    verifyStubbedInvocationsAreUsed()
+    unstub()
+
+
+def version_info(filename: str) -> VersionInfo:
+    return VersionInfo(
+        stability=Stabilities.release,
+        mod_loaders=set(),
+        site=Sites.curse,
+        upload_time=1,
+        minecraft_versions=[],
+        download_url="",
+        filename=filename,
+    )
+
+
+def download_info(old: Union[str, None], new: str) -> DownloadInfo:
+    return DownloadInfo(Mod("", "", file=old), version_info(new))
+
+
+@pytest.mark.parametrize(
+    "name,input,pretend,expected",
+    [
+        (
+            "Remove file when new file has been downloaded",
+            download_info("old", "new"),
+            False,
+            True,
+        ),
+        (
+            "Keep file when no new file has been downloaded",
+            download_info("old", "old"),
+            False,
+            False,
+        ),
+        (
+            "Keep old file when new filename is empty",
+            download_info("old", ""),
+            False,
+            False,
+        ),
+        (
+            "Don't remove old file when it doesn't exist",
+            download_info(None, "new"),
+            False,
+            False,
+        ),
+        (
+            "Don't remove old file when it's empty",
+            download_info("", "new"),
+            False,
+            False,
+        ),
+        (
+            "Don't remove old file when --pretend is on",
+            download_info("old", "new"),
+            True,
+            False,
+        ),
+    ],
+)
+def test_on_version_found(name: str, input: DownloadInfo, pretend: bool, expected: bool, mock_repo):
+    print(name)
+
+    config.pretend = pretend
+    if expected:
+        when(mock_repo).remove_mod_file(input.mod.file)
+
+    update = Update(mock_repo)
+    update.on_version_found(input)
+
+    config.pretend = False
     verifyStubbedInvocationsAreUsed()
     unstub()
