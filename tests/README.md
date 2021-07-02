ca0bec4efcda037ed4b0bbd4f8f460bfd88a8ce0
Senth <senth.wallace@gmail.com>
2021-07-02 22:57:22 +0200

    ✅ Add installation integration tests

---
+++ tests/README.md
@@ -0,0 +1,5 @@
+# Integration Tests
+
+This folder contains all the integration tests for minecraft mod manager.
+It makes sure that the `minecraft-mod-manager` command works correctly
+by testing various scenarios.
---
+++ tests/install_test.py
@@ -0,0 +1,23 @@
+from .helper import Helper, helper
+
+
+def test_reinstall_mod_after_manual_removal(helper: Helper):
+    code = helper.run("install", "carpet")
+    carpet_mod = helper.get_mod_in_dir_like("*carpet*.jar")
+    assert carpet_mod is not None
+    assert code == 0
+
+    carpet_mod.unlink()
+
+    code = helper.run("install", "carpet")
+    carpet_mod = helper.get_mod_in_dir_like("*carpet*.jar")
+    assert carpet_mod is not None
+    assert code == 0
+
+
+def test_install_mod_that_has_no_mod_loader(helper: Helper):
+    code = helper.run("install", "jei")
+    jei = helper.get_mod_in_dir_like("*jei*.jar")
+
+    assert code == 0
+    assert jei is not None
