818a849e5b9f45802908b06e12b5a31c88906563
Senth <senth.wallace@gmail.com>
2021-07-02 23:22:32 +0200

    👌🏻 ✅ Add update tests to test so that mods are not deleted

---
+++ tests/update_test.py
@@ -0,0 +1,15 @@
+from .helper import Helper, helper  # lgtm[py/unused-import]
+
+
+def test_update_does_not_remove_mods(helper: Helper):
+    helper.run("install", "carpet", "-v", "1.16.2")
+    carpet_mod = helper.get_mod_in_dir_like("*carpet*.jar")
+    assert carpet_mod is not None
+
+    helper.run("update")
+    carpet_mod = helper.get_mod_in_dir_like("*carpet*.jar")
+    assert carpet_mod is not None
+
+    helper.run("update")
+    carpet_mod = helper.get_mod_in_dir_like("*carpet*.jar")
+    assert carpet_mod is not None
