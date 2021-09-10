5ab345832c82a9d75a5262fd663371eae90b0ffa
Senth <senth.wallace@gmail.com>
2021-09-10 20:18:52 +0200

    ✨💄 Add kivy GUI that starts when no args
    Fix #96

--- minecraft_mod_manager/__main__.py
+++ minecraft_mod_manager/__main__.py
@@ -1,42 +1,19 @@
-from .adapters.repo_impl import RepoImpl
-from .app.configure.configure import Configure
-from .app.install.install import Install
-from .app.show.show import Show
-from .app.update.update import Update
-from .config import config
-from .core.entities.actions import Actions
-from .gateways.arg_parser import parse_args
-from .gateways.downloader import Downloader
-from .gateways.jar_parser import JarParser
-from .gateways.sqlite import Sqlite
+import sys
+
+from .interfaces import cli
+
+
+def should_start_gui() -> bool:
+    return len(sys.argv) == 1
 
 
 def main():
-    args = parse_args()
-    config.add_arg_settings(args)
+    if should_start_gui():
+        from .interfaces.gui.window import Window
 
-    sqlite = Sqlite()
-    jar_parser = JarParser(config.dir)
-    downloader = Downloader()
-    repo = RepoImpl(jar_parser, sqlite, downloader)
-    try:
-        if config.action == Actions.update:
-            update = Update(repo)
-            update.execute(config.arg_mods)
-
-        elif config.action == Actions.install:
-            install = Install(repo)
-            install.execute(config.arg_mods)
-
-        elif config.action == Actions.configure:
-            configure = Configure(repo)
-            configure.execute(config.arg_mods)
-
-        elif config.action == Actions.list:
-            show = Show(repo)
-            show.execute()
-    finally:
-        sqlite.close()
+        Window().run()
+    else:
+        cli.run()
 
 
 if __name__ == "__main__":
