6c9bd683791ef5db1cd98c9b0de6eed5f39fc294
Senth <senth.wallace@gmail.com>
2022-02-21 20:48:41 +0100

    ✅ Add modrinth download

--- tests/install_test.py
+++ tests/install_test.py
@@ -1,6 +1,6 @@
-from .util.helper import Helper
-
 import pytest
+
+from .util.helper import Helper
 
 
 @pytest.fixture
@@ -41,3 +41,12 @@ def test_remember_slug_for_installed_mod_if_mod_id_varies(helper: Helper):
 
     assert code == 0
     assert helper.exists_in_db("unearthed")
+
+
+def test_install_from_modrinth(helper: Helper):
+    code = helper.run("install", "lithium=modrinth:lithium")
+    lithium_mod = helper.get_mod_in_dir_like("*lithium*.jar")
+    assert lithium_mod is not None
+    assert code == 0
+
+    lithium_mod.unlink()
