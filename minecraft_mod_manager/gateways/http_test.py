4136cb33d38ec6eed8cd6dc6fcfe40c23fc2312a
Senth <senth.wallace@gmail.com>
2022-04-19 22:09:06 +0200

    🐛 ✅ Fix if http response is plain/text

--- minecraft_mod_manager/gateways/http_test.py
+++ minecraft_mod_manager/gateways/http_test.py
@@ -6,7 +6,6 @@ import pytest
 import requests
 from mockito import mock, unstub, when
 from requests.models import Response
-from requests.structures import CaseInsensitiveDict
 
 from ..config import config
 from ..core.errors.download_failed import DownloadFailed
@@ -14,13 +13,22 @@ from .http import Http, MaxRetriesExceeded
 
 
 @pytest.fixture
+def response():
+    response = Response()
+    response.status_code = 200
+    response.encoding = "UTF-8"
+    response._content = b""  # type:ignore
+    return response
+
+
+@pytest.fixture
 def mock_response():
-    mock_response = mock(Response)
-    mock_response.status_code = 200  # type:ignore
-    mock_response.content = ""  # type:ignore
-    when(mock_response).__enter__(...).thenReturn(mock_response)
-    when(mock_response).__exit__(...)
-    return mock_response
+    response = mock(Response)
+    response.status_code = 200  # type:ignore
+    response.content = ""  # type:ignore
+    when(response).__enter__(...).thenReturn(response)
+    when(response).__exit__(...)
+    return response
 
 
 @pytest.fixture
@@ -37,49 +45,43 @@ def http():
     return Http()
 
 
-def test_use_filename_when_it_exists(http, mock_response, mock_file):
+def test_use_filename_when_it_exists(http, response, mock_file):
     filename = "some-file.jar"
     expected = path.join(config.dir, filename)
 
-    when(requests).get(...).thenReturn(mock_response)
+    when(requests).get(...).thenReturn(response)
     when(builtins).open(...).thenReturn(mock_file)
 
-    result = http.download("", filename)
+    actual = http.download("", filename)
 
     unstub()
-    assert expected == result
+    assert expected == actual
 
 
-def test_use_downloaded_filename_when_no_filename_specified(http, mock_response, mock_file):
-    mock_headers = mock(CaseInsensitiveDict)
-    mock_response.headers = mock_headers
+def test_use_downloaded_filename_when_no_filename_specified(http, response, mock_file):
     filename = "downloaded.jar"
     expected = path.join(config.dir, filename)
-
-    when(mock_headers).get(...).thenReturn(f"filename={filename}")
-    when(requests).get(...).thenReturn(mock_response)
+    response.headers["Content-Disposition"] = f"attachment; filename={filename}"
+    when(requests).get(...).thenReturn(response)
     when(builtins).open(...).thenReturn(mock_file)
 
-    result = http.download("", "")
+    actual = http.download("", "")
 
     unstub()
-    assert expected == result
+    assert expected == actual
 
 
-def test_use_downloaded_filename_add_jar_when_no_filename_specified(http, mock_response, mock_file):
-    mock_headers = mock(CaseInsensitiveDict)
-    mock_response.headers = mock_headers
-    filename = "downloaded"
-    expected = path.join(config.dir, filename + ".jar")
-
-    when(mock_headers).get(...).thenReturn(f"filename={filename}")
-    when(requests).get(...).thenReturn(mock_response)
+def test_use_downloaded_filename_add_jar_when_no_filename_specified(http, response, mock_file):
+    filename = "downloaded.jar"
+    expected = path.join(config.dir, filename)
+    response.headers["Content-Disposition"] = f"attachment; filename={filename}"
+    when(requests).get(...).thenReturn(response)
     when(builtins).open(...).thenReturn(mock_file)
 
-    result = http.download("", "")
+    actual = http.download("", "")
 
     unstub()
-    assert expected == result
+    assert expected == actual
 
 
 def test_no_mock_interactions_when_pretending(http):
@@ -90,8 +92,8 @@ def test_no_mock_interactions_when_pretending(http):
     when(builtins).open(...).thenRaise(NotImplementedError())
 
     try:
-        result = http.download("", filename)
-        assert expected == result
+        actual = http.download("", filename)
+        assert expected == actual
     except Exception as e:
         assert e is None
     finally:
@@ -99,26 +101,45 @@ def test_no_mock_interactions_when_pretending(http):
         unstub()
 
 
-def test_get_when_non_strict_json(http):
-    response = Response()
-    response.encoding = "UTF-8"
-    response.status_code = 200
+def test_get_when_non_strict_json(http, response):
+    response.headers["Content-Type"] = "application/json"
     response._content = '\n{"text":"This is\nmy text"}'.encode("UTF-8")
     expected = {"text": "This is\nmy text"}
     when(requests).get(...).thenReturn(response)
 
-    result = http.get("https://test.com")
+    actual = http.get("https://test.com")
 
-    assert expected == result
+    assert expected == actual
 
     unstub()
 
 
-def test_download_failed(http, mock_response):
-    mock_response.status_code = 404  # type:ignore
-    mock_response.reason = "Not found"  # type:ignore
-    mock_response.content = "404 not found"  # type:ignore
-    when(requests).get(...).thenReturn(mock_response)
+def test_get_when_response_is_string(http, response):
+    response.headers["Content-Type"] = "text/plain"
+    expected = "This is my text"
+    response._content = b"This is my text"  # type:ignore
+    when(requests).get(...).thenReturn(response)
+
+    actual = http.get("https://test.com")
+
+    assert expected == actual
+
+
+def test_get_when_content_type_is_missing(http, response):
+    expected = "This is my text"
+    response._content = b"This is my text"  # type:ignore
+    when(requests).get(...).thenReturn(response)
+
+    actual = http.get("https://test.com")
+
+    assert expected == actual
+
+
+def test_download_failed(http, response):
+    response.status_code = 404  # type:ignore
+    response.reason = "Not found"  # type:ignore
+    response._content = "404 not found"  # type:ignore
+    when(requests).get(...).thenReturn(response)
 
     with pytest.raises(DownloadFailed):
         http.download("", "")
@@ -129,9 +150,8 @@ def test_download_failed(http, mock_response):
 def test_download_retry(http, mock_response):
     mock_response.status_code = 524  # type:ignore
     mock_response.reason = "Timed out"  # type:ignore
-    mock_response.content = "524 Timed out"  # type:ignore
-    when(mock_response).close()
-
+    mock_response._content = "524 Timed out"  # type:ignore
+    when(mock_response).close(...)
     when(requests).get(...).thenReturn(mock_response)
 
     with pytest.raises(MaxRetriesExceeded):
