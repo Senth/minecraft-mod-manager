19664b88f759f85e98190c13655438abb4db7893
Senth <senth.wallace@gmail.com>
2022-05-05 21:37:30 +0200

    ✅♻️ Convert to table-tests

--- minecraft_mod_manager/app/download/download_test.py
+++ minecraft_mod_manager/app/download/download_test.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Any
 
 import pytest
 from mockito import mock, unstub, verifyStubbedInvocationsAreUsed, when
@@ -13,18 +14,12 @@ from .download import Download
 from .download_repo import DownloadRepo
 
 
-@pytest.fixture
-def mock_repo():
-    return mock(DownloadRepo)
+class T:
+    """Used for testing the Download class. And making use of default values for objects"""
 
-
-@pytest.fixture
-def mock_finder():
-    return mock(ModFinder)
-
-
-def test_download_and_install_when_found(mock_repo, mock_finder):
-    input = [Mod("found", "")]
+    mock_repo: Any
+    mock_finder: Any
+    download: Download
     version_info = VersionInfo(
         stability=Stabilities.release,
         mod_loaders=set([ModLoaders.fabric]),
@@ -33,71 +28,60 @@ def test_download_and_install_when_found(mock_repo, mock_finder):
         minecraft_versions=[],
         download_url="",
     )
-    when(mock_finder).find_mod(...).thenReturn({Sites.curse: Site(Sites.curse, "", "")})
-    when(mock_repo).get_versions(...).thenReturn([version_info])
-    when(mock_repo).download(...).thenReturn(Path("mod.jar"))
-    when(mock_repo).get_mod_from_file(...).thenReturn(Mod("found", ""))
-    when(mock_repo).update_mod(...)
-
-    download = Download(mock_repo, mock_finder)
-    when(download).on_version_found(...)
-    download.find_download_and_install(input)
-
-    verifyStubbedInvocationsAreUsed()
-    unstub()
 
 
-def test_download_and_install_remove_downloaded_file_when_invalid_mod_file(mock_repo, mock_finder):
-    input = [Mod("found", "")]
-    version_info = VersionInfo(
-        stability=Stabilities.release,
-        mod_loaders=set([ModLoaders.fabric]),
-        site=Sites.curse,
-        upload_time=0,
-        minecraft_versions=[],
-        download_url="",
-    )
-    when(mock_finder).find_mod(...).thenReturn({Sites.curse: Site(Sites.curse, "", "")})
-    when(mock_repo).get_versions(...).thenReturn([version_info])
-    when(mock_repo).download(...).thenReturn(Path("mod.jar"))
-    when(mock_repo).get_mod_from_file(...).thenRaise(ModFileInvalid(Path("mod.jar")))
-    when(mock_repo).remove_mod_file(...)
+@pytest.mark.parametrize(
+    "name,prepare_function",
+    [
+        (
+            "Call on_version_found() when found",
+            lambda: (
+                when(T.mock_repo).get_versions(...).thenReturn([T.version_info]),
+                when(T.mock_repo).get_mod_from_file(...).thenReturn(Mod("found", "")),
+                when(T.mock_repo).update_mod(...),
+                when(T.download).on_version_found(...),
+            ),
+        ),
+        (
+            "Remove downloaded file when invalid mod file",
+            lambda: (
+                when(T.mock_repo).get_versions(...).thenReturn([T.version_info]),
+                when(T.mock_repo).get_mod_from_file(...).thenRaise(ModFileInvalid(Path("mod.jar"))),
+                when(T.mock_repo).remove_mod_file(...),
+            ),
+        ),
+        (
+            "Update mod_id after download",
+            lambda: (
+                when(T.mock_repo).get_versions(...).thenReturn([T.version_info]),
+                when(T.mock_repo).update_mod(
+                    Mod(
+                        "validid",
+                        "name",
+                        sites={Sites.curse: Site(Sites.curse, "", "")},
+                        version="1.0.0",
+                        file="mod.jar",
+                    )
+                ),
+                when(T.mock_repo).get_mod_from_file("mod.jar").thenReturn(Mod("validid", "name", version="1.0.0")),
+                when(T.download).on_version_found(...),
+            ),
+        ),
+    ],
+)
+def test_find_download_and_install(name, prepare_function):
+    print(name)
 
-    download = Download(mock_repo, mock_finder)
-    download.find_download_and_install(input)
+    input = [Mod("just-some-mod-id", "")]
 
-    verifyStubbedInvocationsAreUsed()
-    unstub()
+    T.mock_repo = mock(DownloadRepo)
+    when(T.mock_repo).download(...).thenReturn(Path("mod.jar"))
+    T.mock_finder = mock(ModFinder)
+    when(T.mock_finder).find_mod(...).thenReturn({Sites.curse: Site(Sites.curse, "", "")})
+    T.download = Download(T.mock_repo, T.mock_finder)
 
-
-def test_update_mod_id_after_download(mock_repo, mock_finder):
-    download = Download(mock_repo, mock_finder)
-
-    mod_arg = Mod("dummy", "name")
-    installed_mod = Mod("validid", "name", version="1.0.0")
-    expected_mod = Mod(
-        "validid",
-        "name",
-        sites={Sites.curse: Site(Sites.curse, "", "")},
-        version="1.0.0",
-        file="mod.jar",
-    )
-    version_info = VersionInfo(
-        stability=Stabilities.release,
-        mod_loaders={ModLoaders.fabric},
-        site=Sites.curse,
-        upload_time=0,
-        minecraft_versions=[],
-        download_url="",
-    )
-    when(mock_finder).find_mod(...).thenReturn({Sites.curse: Site(Sites.curse, "", "")})
-    when(mock_repo).get_versions(...).thenReturn([version_info])
-    when(mock_repo).download(...).thenReturn(Path("mod.jar"))
-    when(mock_repo).update_mod(expected_mod)
-    when(mock_repo).get_mod_from_file("mod.jar").thenReturn(installed_mod)
-    when(download).on_version_found(...)
-
-    download.find_download_and_install([mod_arg])
+    prepare_function()
+    T.download.find_download_and_install(input)
 
     verifyStubbedInvocationsAreUsed()
     unstub()
