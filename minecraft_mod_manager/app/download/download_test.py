3654fd998f85bc3c48cf36939eb4c315e80f6452
Senth <senth.wallace@gmail.com>
2022-05-05 22:06:57 +0200

    ✨✅ Download dependencies

--- minecraft_mod_manager/app/download/download_test.py
+++ minecraft_mod_manager/app/download/download_test.py
@@ -17,6 +17,7 @@ from .download_repo import DownloadRepo
 class T:
     """Used for testing the Download class. And making use of default values for objects"""
 
+    input = [Mod("just-some-mod-id", "")]
     mock_repo: Any
     mock_finder: Any
     download: Download
@@ -67,12 +68,41 @@ class T:
                 when(T.download).on_version_found(...),
             ),
         ),
+        (
+            "Download dependency",
+            lambda: (
+                when(T.mock_repo)
+                .get_versions(T.input[0])
+                .thenReturn(
+                    [
+                        VersionInfo(
+                            stability=Stabilities.release,
+                            mod_loaders=set([ModLoaders.fabric]),
+                            site=Sites.curse,
+                            upload_time=0,
+                            minecraft_versions=[],
+                            download_url="",
+                            dependencies={Sites.curse: ["123", "456"]},
+                            filename="parent.jar",
+                        )
+                    ]
+                ),
+                when(T.mock_repo)
+                .get_versions(Mod("123", "123 Name", {Sites.curse: Site(Sites.curse, "", "")}))
+                .thenReturn([T.version_info]),
+                when(T.mock_finder).get_mod_info(Sites.curse, "123").thenReturn(Mod("123", "123 Name")),
+                when(T.mock_finder).get_mod_info(Sites.curse, "456").thenReturn(None),
+                when(T.mock_repo).download("", "parent.jar").thenReturn(Path("parent.jar")),
+                when(T.mock_repo).get_mod_from_file("mod.jar").thenReturn(Mod("123", "123 Name")),
+                when(T.mock_repo).get_mod_from_file("parent.jar").thenReturn(T.input[0]),
+                when(T.mock_repo).update_mod(...),
+                when(T.download).on_version_found(...),
+            ),
+        ),
     ],
 )
 def test_find_download_and_install(name, prepare_function):
     print(name)
-
-    input = [Mod("just-some-mod-id", "")]
 
     T.mock_repo = mock(DownloadRepo)
     when(T.mock_repo).download(...).thenReturn(Path("mod.jar"))
@@ -81,7 +111,7 @@ def test_find_download_and_install(name, prepare_function):
     T.download = Download(T.mock_repo, T.mock_finder)
 
     prepare_function()
-    T.download.find_download_and_install(input)
+    T.download.find_download_and_install(T.input)
 
     verifyStubbedInvocationsAreUsed()
     unstub()
